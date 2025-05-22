6e88ee8cd790055eecab5ff4526ea34cf26934ea
Robin Bryce <robinbryce@gmail.com>
2025-05-22 09:57:41 +0100

    Basic (lightweight) lazy vim setup

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+
+-- global config
+vim.opt.autowrite = true -- write file whenever you get taken elsewhere
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+require("config.lazy")
+
+-- global keymap
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
