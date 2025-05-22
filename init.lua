bceb50f686b5d75366bcb29d6af2b59f3960c237
Robin Bryce <robinbryce@gmail.com>
2025-05-22 13:44:07 +0100

    feat: add co-pilot, and blink-cmp-copilot fix: suppress neotest warning

--- init.lua
+++ init.lua
@@ -7,19 +7,7 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 require("config.lazy")
-
---- hacks & workarounds
-
---- known issue with the neotest plugin and how it invokes nvim_create_autocmd
---- suppress all errors like - E5560: nvim_create_autocmd must not be called in a fast event context
---- in the case of neotest, they are completley benign and safely ignored
-local orig_notify = vim.notify
-vim.notify = function(msg, level, opts)
-  if msg:match("nvim_create_autocmd must not be called in a fast event context") then
-    return
-  end
-  orig_notify(msg, level, opts)
-end
+require("config.suppress_warnings")
 
 -- global keymap
 vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
