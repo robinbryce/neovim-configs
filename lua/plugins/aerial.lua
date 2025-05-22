b0132443c8907a56364ef492616a87ae1690ffd0
Robin Bryce <robinbryce@gmail.com>
2025-05-22 10:01:13 +0100

    Base fullstack (web) dev setup

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "stevearc/aerial.nvim",
+    -- opts will be merged with the parent spec
+    -- https://github.com/stevearc/aerial.nvim?tab=readme-ov-file#options
+    opts = {
+      manage_folds = true,
+    },
+  },
+}
